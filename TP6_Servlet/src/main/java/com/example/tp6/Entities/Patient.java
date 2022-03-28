9babfc1f7e01bc9ca5fe46b9dfc56cbec8342452
SoukainaElkm <80215968+SoukainaElkm@users.noreply.github.com>
2022-03-28 21:42:13 +0000

    Add files via upload

---
+++ TP6_Servlet/src/main/java/com/example/tp6/Entities/Patient.java
@@ -0,0 +1,22 @@
+package com.example.tp6.Entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @Temporal(TemporalType.DATE)
+    private Date datenaissance;
+    private boolean malade;
+    private int score;
+}
